d90924f1aa383b5a2f1b3caa5b1d4963b5033e7a
washtakzo <washtakzo@gmail.com>
2023-08-01 21:06:24 +0200

    store context init

---
+++ store/card-context.tsx
@@ -0,0 +1,89 @@
+import React, { createContext, useState } from "react";
+
+type CardValues = {
+  products: Product[];
+  addProduct: (product: Product) => void;
+  deleteProduct: (product: Product) => void;
+};
+
+type Product = {
+  trappe: Trappe;
+  surface: number;
+  quantity: number;
+};
+
+const initialValues: CardValues = {
+  products: [],
+  addProduct: (product: Product) => {},
+  deleteProduct: (product: Product) => {},
+};
+
+export const CardContext = createContext(initialValues);
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const CardContextProvider = ({ children }: Props) => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const addProduct = (product: Product) => {
+    const productsCopy = [...products];
+
+    const productIsAlreadyInCard =
+      productsCopy.findIndex(
+        (p) =>
+          p.trappe.id === product.trappe.id && p.surface === product.surface
+      ) >= 0;
+
+    if (productIsAlreadyInCard) {
+      const updatedProducts = productsCopy.map((p) => {
+        if (p.trappe.id === product.trappe.id) {
+          return {
+            ...p,
+            quantity: p.quantity + 1,
+          };
+        }
+        return p;
+      });
+
+      setProducts(updatedProducts);
+    } else {
+      setProducts((state) => [...state, product]);
+    }
+  };
+
+  const deleteProduct = (product: Product) => {
+    const productsCopy = [...products];
+
+    const productIsAlreadyInCard =
+      productsCopy.findIndex(
+        (p) =>
+          p.trappe.id === product.trappe.id && p.surface === product.surface
+      ) >= 0;
+
+    if (productIsAlreadyInCard) {
+      const updatedProducts = productsCopy.map((p) => {
+        if (p.trappe.id === product.trappe.id) {
+          return {
+            ...p,
+            quantity: p.quantity - 1,
+          };
+        }
+        return p;
+      });
+
+      const filteredProducts = updatedProducts.filter((p) => p.quantity > 0);
+
+      setProducts(filteredProducts);
+    }
+  };
+
+  const value = {
+    products,
+    addProduct,
+    deleteProduct,
+  };
+
+  return <CardContext.Provider value={value}>{children}</CardContext.Provider>;
+};
