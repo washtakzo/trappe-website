f10bdfe0db639908573c0e59ce4824e530c55523
washtakzo <washtakzo@gmail.com>
2023-10-07 20:24:48 +0200

    add post trappe test button

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,34 @@ export default function Home({
   console.log(process.env.API_BASE_URL);
   console.log({ trappes });
   console.log({ log });
+
+  //TODO:delete
+  //TODELETE:test
+  const postTrappeHandler = () => {
+    const trappeToPost = {
+      name: "Trappe Electrique789",
+      short_description: "Trappe pour accès éléctrique",
+      long_description:
+        "Trappe de qualité allemande pour les regards en board de routes",
+      min_width: 180,
+      max_width: 260,
+      min_length: 190,
+      max_length: 888,
+      height: 13,
+      images: [
+        "https://in-elec.com/wp-content/uploads/2016/08/ELSA_Regard_1-550x400.jpg",
+        "https://fr.jqcomposites.com/uploadfile/202008/25/9ce513ffc4158b0d68f6af970e92a3a2_medium.jpg",
+      ],
+      prices: [
+        { surface: 100, pricePerMetter: 2.0 },
+        { surface: 800, pricePerMetter: 1.9 },
+        { surface: 1200, pricePerMetter: 1.7 },
+      ],
+      setup_price: 100,
+      shipping_price: 150,
+    };
+    postTrappe(trappeToPost);
+  };
   return (
     <div>
       <Head>
@@ -34,6 +62,7 @@ export default function Home({
       </Head>
 
       <main>
+        <button onClick={postTrappeHandler}>POST TRAPPE</button>
         <Header />
         <SectionIntroduction />
         <SectionMainProducts trappes={trappes} />
